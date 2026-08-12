9b8065d1658efd93c3e56aa7acc4de9b3f0b98a9
lmq8267 <lmq8267@users.noreply.github.com>
2026-08-12 13:52:16 +0800

    修复部分系统插件无法启动的问题

--- luci-app-uugamebooster/luasrc/model/cbi/uuplugin.lua
+++ luci-app-uugamebooster/luasrc/model/cbi/uuplugin.lua
@@ -1,8 +1,9 @@
+local mp, s ,o
 require("luci.util")
 
 mp = Map("uuplugin")
 mp.title = translate("网易UU游戏加速器")
-mp.description = translate("一个富家子弟用的主机游戏加速器 (开启服务后，使用UU主机加速APP绑定路由器并指定加速主机、电脑、手机)")
+mp.description = translate("一个富家子弟用的主机游戏加速器 (开启服务后，使用UU主机加速APP绑定路由器并指定加速主机、电脑、手机)   <a href=\"https://github.com/lmq8267/luci-app-uugamebooster/releases\" target=\"_blank\"><img alt=\"\" src=\"https://img.shields.io/github/v/release/lmq8267/luci-app-uugamebooster?logo=openwrt&label=luci-app-uugamebooster&link=https%3A%2F%2Fgithub.com%2Flmq8267%2Fluci-app-uugamebooster%2Freleases\" style=\"vertical-align:middle;\"></a>")
 
 mp:section(SimpleSection).template  = "uuplugin/uuplugin_status"
 
@@ -12,7 +13,6 @@ s.addremove = false
 
 o = s:option(Flag, "enabled", translate("启用"))
 o.default = 0
-o.optional = false
 
 o = s:option(Value, "model", translate("设备型号"),
         translate("本机设备型号，在APP里方便区分不同路由器，绑定后修改型号需要解绑后重新绑定才生效"))
@@ -20,5 +20,10 @@ o.placeholder = "OpenWrt"
 
 
 mp:section(SimpleSection).template  = "uuplugin/uuplugin_qcode"
+
+mp.apply_on_parse = true
+mp.on_after_apply = function(self,map)
+	luci.sys.exec("/etc/init.d/uuplugin restart")
+end
 
 return mp
